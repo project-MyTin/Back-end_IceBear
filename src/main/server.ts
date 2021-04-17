1abd1721632b24ef4fa87e05a701dae5482b1ddb
pangcurry <magicshuffle2424@hotmail.com>
2021-04-17 11:23:55 +0900

    [Add] add mongodb connect in server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,10 @@
 import env from './config/env';
+import { MongoHelper } from '../infra/db/mongo-helper';
 
-const runServer = async() => {
-    const app = (await import('./config/app')).default;
-    app.listen(env.server.port, () => console.log(`server open on ${env.server.port} !!!`));
-}
-
-runServer();
+MongoHelper.connect(env.mongodb.uri)
+    .then(async() => {
+            const app = (await import('./config/app')).default;
+            app.listen(env.server.port, () => console.log(`server open on ${env.server.port} !!!`));
+        }
+    )
+    .catch(console.error);
