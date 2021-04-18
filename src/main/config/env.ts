93acc7eda3a8f4cd9d07933b9378877b028c7b14
pangcurry <magicshuffle2424@hotmail.com>
2021-04-18 22:17:30 +0900

    [Add] add env config aws

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -11,5 +11,11 @@ export default {
     },
     winston: {
         logDir: __dirname + '/../../../logs',
+    },
+    aws: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+        region: process.env.AWS_REGION,
+        bucket: process.env.AWS_BUCKET_NAME
     }
 }
