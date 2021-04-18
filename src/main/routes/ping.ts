eab75922a3159a2fb6e4618c60c31af483192c31
pangcurry <magicshuffle2424@hotmail.com>
2021-04-18 22:18:55 +0900

    [Add] add upload image in ping route

--- src/main/routes/ping.ts
+++ src/main/routes/ping.ts
@@ -1,9 +1,18 @@
 import { Request, Response, Router } from "express";
+import { upload } from '../config/multer';
 
 export default (router: Router): void => {
-    router.get('/ping', (req:Request,res:Response) => {
+    router.get('/ping', upload.single('img'), (req:Request,res:Response) => {
+        const key = req.file["key"];
+        console.log('====================');
+        // console.log(req.body.hi);
+        if(req.file !== undefined) {
+            // res.status(400).json({
+            //     message: "image Does not exist."
+            // });
+        }
         res.status(200).json({
-            pong:"pong"
+            pong: key
         });
     });
 }
