c3d12bb935656d1c89806f46dbdcca8e5551e92c
pangcurry <magicshuffle2424@hotmail.com>
2021-04-20 23:12:18 +0900

    [Fix] fix test route ping

--- src/main/routes/ping.ts
+++ src/main/routes/ping.ts
@@ -1,18 +1,28 @@
 import { Request, Response, Router } from "express";
+import { MongoHelper } from "../../infra/db/mongo-helper";
 import { upload } from '../config/multer';
 
 export default (router: Router): void => {
-    router.get('/ping', upload.single('img'), (req:Request,res:Response) => {
-        const key = req.file["key"];
-        console.log('====================');
-        // console.log(req.body.hi);
-        if(req.file !== undefined) {
-            // res.status(400).json({
-            //     message: "image Does not exist."
-            // });
+    router.get('/ping', upload.single('img'), async (req:Request,res:Response) => {
+        try {
+            const key = req.file["key"];
+            console.log('====================');
+            // console.log(req.body.hi);
+            if(req.file !== undefined) {
+                // res.status(400).json({
+                //     message: "image Does not exist."
+                // });
+            }
+            const Motion = await MongoHelper.loadSchema('Motion');
+            
+            const list = await Motion.find({});
+            res.status(200).json({
+                pong: key,
+                pong2: list
+            });    
+        } catch (err) {
+            console.log(err);
         }
-        res.status(200).json({
-            pong: key
-        });
+        
     });
 }
