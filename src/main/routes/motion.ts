f61adb33c325ee939e20cfa8a93565ddd94cb7a8
pangcurry <magicshuffle2424@hotmail.com>
2021-04-21 14:08:52 +0900

    [Add] add motion route

---
+++ src/main/routes/motion.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { adaptRoute } from "../adapters";
+import { makeMotionPostController } from "../factories/controllers";
+
+export default (router: Router): void => {
+    router.post('/motion', adaptRoute(makeMotionPostController()));
+}
