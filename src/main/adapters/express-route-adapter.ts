4d8885f93cf62e8e5d5a32e83481fef4732a06c8
pangcurry <magicshuffle2424@hotmail.com>
2021-04-21 14:07:20 +0900

    [Add] add adaptroute

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { Controller } from "../../presentation/protocols";
+
+export const adaptRoute = (controller: Controller) => {
+    return async (req: Request, res: Response) => {
+        const request = {
+            ...(req.body || {}),
+            ...(req.params || {}),
+            ...(req.query || {})
+        };
+        const httpResponse = await controller.handle(request);
+        const { status, statusCode, message, data } = httpResponse;
+        if(200 <= status && status <= 299) {
+            res.status(status).json({ statusCode, message, data });
+        } else {
+            res.status(status).json({ statusCode, error: message });
+        }
+    }
+};
