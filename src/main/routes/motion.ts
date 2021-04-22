7c68fd1ea4c9c76c67295489d522341725f26601
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 19:46:10 +0900

    [Add] add uploadSingle middleware at /motion post and put method

--- src/main/routes/motion.ts
+++ src/main/routes/motion.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
 import { upload } from "../config/multer";
-import { makeMotionPostController } from "../factories/controllers";
+import { makeMotionPostController, makeMotionPutController } from "../factories/controllers";
+import { uploadSingle } from "../middlewares";
 
 export default (router: Router): void => {
-    router.post('/motion', upload.single('img'), adaptRoute(makeMotionPostController()));
+    router.post('/motion', upload.single('img'), uploadSingle, adaptRoute(makeMotionPostController()));
+    router.put('/motion/:id', upload.single('img'), uploadSingle, adaptRoute(makeMotionPutController()));
 };
