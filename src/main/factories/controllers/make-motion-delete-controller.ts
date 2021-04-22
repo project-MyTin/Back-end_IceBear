af01fef7584b87ba40ac86a3898d3f63a2fa2f89
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 08:53:45 +0900

    [Add] add logControllerDecorator in motionDeleteController factory

--- src/main/factories/controllers/make-motion-delete-controller.ts
+++ src/main/factories/controllers/make-motion-delete-controller.ts
@@ -1,5 +1,8 @@
+import { MotionDeleteController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
+import { LogControllerDecorator } from "../../decorators";
 
 export const makeMotionDeleteController = (): Controller => {
-    
+    const controller = new MotionDeleteController();
+    return new LogControllerDecorator(controller);
 }
