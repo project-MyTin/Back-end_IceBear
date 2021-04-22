4be1681f1dfb7ba011820cd88c45aed08aa4bf87
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 20:22:59 +0900

    [Fix] fix middleware. add field property in uploadSingleMiddleware contructor

--- src/presentation/middlewares/upload-single-middleware.ts
+++ src/presentation/middlewares/upload-single-middleware.ts
@@ -3,9 +3,13 @@ import { ok } from "../helpers";
 import env from '../../main/config/env';
 
 export class UploadSingleMiddleware implements Middleware {
-    constructor() {}
+    constructor(private readonly field: string) {}
     async handle(request: UploadSingleMiddleware.Request): Promise<HttpResponse> {
-        return ok({ imageKey: 'key' in request ? request.key : env.aws.defaultImage }); // 기본 이미지 경로 전송
+        const key = {
+            post: env.aws.defaultImage,
+            put: null
+        }
+        return ok({ imageKey: 'key' in request ? request.key : key[this.field] }); // 기본 이미지 경로 전송
     }
 }
 
