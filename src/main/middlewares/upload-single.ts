dae36a65292e828b6f090a7a956afef17f502948
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 19:38:18 +0900

    [Add] add uploadSingle middleware

---
+++ src/main/middlewares/upload-single.ts
@@ -0,0 +1,4 @@
+import { adaptMiddleware } from "../adapters";
+import { makeUploadSingleMiddleware } from "../factories/middlewares";
+
+export const uploadSingle = adaptMiddleware(makeUploadSingleMiddleware());
