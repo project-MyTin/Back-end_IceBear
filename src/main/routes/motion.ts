0a3166f15c5271dadf385d6c8d3fd9fb6bf2840a
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 20:25:25 +0900

    [Fix] fix uploadSingle to uploadSingleUpdate in /motion put method

--- src/main/routes/motion.ts
+++ src/main/routes/motion.ts
@@ -3,8 +3,9 @@ import { adaptRoute } from "../adapters";
 import { upload } from "../config/multer";
 import { makeMotionPostController, makeMotionPutController } from "../factories/controllers";
 import { uploadSingle } from "../middlewares";
+import { uploadSingleUpdate } from "../middlewares/upload-single-update";
 
 export default (router: Router): void => {
     router.post('/motion', upload.single('img'), uploadSingle, adaptRoute(makeMotionPostController()));
-    router.put('/motion/:id', upload.single('img'), uploadSingle, adaptRoute(makeMotionPutController()));
+    router.put('/motion/:id', upload.single('img'), uploadSingleUpdate, adaptRoute(makeMotionPutController()));
 };
