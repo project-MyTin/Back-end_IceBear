bef3504bbfd55077a84faa7d99be3fe05697daea
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 08:55:11 +0900

    [Add] add motionDeleteController factory in motion route

--- src/main/routes/motion.ts
+++ src/main/routes/motion.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
 import { upload } from "../config/multer";
-import { makeMotionPostController, makeMotionPutController } from "../factories/controllers";
+import { makeMotionDeleteController, makeMotionPostController, makeMotionPutController } from "../factories/controllers";
 import { uploadSingle } from "../middlewares";
 import { uploadSingleUpdate } from "../middlewares/upload-single-update";
 
 export default (router: Router): void => {
     router.post('/motion', upload.single('img'), uploadSingle, adaptRoute(makeMotionPostController()));
     router.put('/motion/:id', upload.single('img'), uploadSingleUpdate, adaptRoute(makeMotionPutController()));
-    router.delete('/motion/:id', adaptRoute());
+    router.delete('/motion/:id', adaptRoute(makeMotionDeleteController()));
 };
