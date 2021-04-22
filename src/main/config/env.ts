176f5523c5b889e85212af6556c0919577f08c5f
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 19:35:14 +0900

    [Add] add defaultImage in env config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -16,6 +16,7 @@ export default {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_KEY,
         region: process.env.AWS_REGION,
-        bucket: process.env.AWS_BUCKET_NAME
+        bucket: process.env.AWS_BUCKET_NAME,
+        defaultImage: process.env.AWS_DEFAULT_IMAGE
     }
 }
