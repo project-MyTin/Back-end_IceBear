da56c06455c8e50471935b2dbe7eacd61b02b249
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 10:14:44 +0900

    [Add] add upload middleware in post /motion

--- src/main/routes/motion.ts
+++ src/main/routes/motion.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
+import { upload } from "../config/multer";
 import { makeMotionPostController } from "../factories/controllers";
 
 export default (router: Router): void => {
-    router.post('/motion', adaptRoute(makeMotionPostController()));
+    router.post('/motion', upload.single('img'), adaptRoute(makeMotionPostController()));
 };
