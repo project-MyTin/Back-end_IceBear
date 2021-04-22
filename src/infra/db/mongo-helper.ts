404473095d8b3188a16bfc9a58082ecaca51aceb
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 23:24:01 +0900

    [Add] add logging function in mongo-helper connect func

--- src/infra/db/mongo-helper.ts
+++ src/infra/db/mongo-helper.ts
@@ -1,5 +1,6 @@
-import mongoose, { Connection, Document, Model, Mongoose } from 'mongoose';
+import mongoose, { Connection, Document, Model } from 'mongoose';
 import autoIncrement from 'mongoose-auto-increment';
+import { logger } from '../../main/config/winston';
 
 export const MongoHelper = {
     client: null as Connection,
@@ -13,33 +14,12 @@ export const MongoHelper = {
                 useFindAndModify: false
             });
             autoIncrement.initialize(this.client);
-
+            logger.info("Connection successful to MongoDB !!!");
             console.log("Connection successful to MongoDB !!!");
-            // const Motion = this.client.model('Motion', MotionSchema);
-            
-            // const input = new Motion({
-            //     motion_id: 987654321,
-            //     file: 'test file222',
-            //     name: 'test name222',
-            //     time: 1,
-            //     parts: ['test part222','hahahahahaha']
-            // });
-            // await input.save();
-            // console.log(input);
-            // const result = await Motion.find({});
-            // console.log(result);
-            // User.findById();// ex)
-            // 클래스 만들기 => 스키마 정의파일에서 윗줄처럼 사용가능하도록 빼주기
-            // 클래스에서 생성자로 conn 받기 => 그래야 각 파일에서 모듈로 보내줄 수 있을 것이다.
-            // this.client = await mongoose.connect(uri, {
-            //     useNewUrlParser: true,
-            //     useCreateIndex: true,
-            //     useUnifiedTopology: true,
-            // });
-            
         }
         catch(err) {
-            console.log(err);   // 에러 로깅하기
+            logger.error(err);
+            console.log(err);  // 에러 환경에 따라 콘솔,.log 구분하기
         }
     },
 
