9d3247a3a0ed507b3565b055d24f877168751e0a
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 20:24:26 +0900

    [Add] add field property

--- src/main/factories/middlewares/upload-single-middleware-factory.ts
+++ src/main/factories/middlewares/upload-single-middleware-factory.ts
@@ -1,6 +1,6 @@
 import { UploadSingleMiddleware } from "../../../presentation/middlewares";
 import { Middleware } from "../../../presentation/protocols";
 
-export const makeUploadSingleMiddleware = (): Middleware => {
-    return new UploadSingleMiddleware();
+export const makeUploadSingleMiddleware = (field: string): Middleware => {
+    return new UploadSingleMiddleware(field);
 }
