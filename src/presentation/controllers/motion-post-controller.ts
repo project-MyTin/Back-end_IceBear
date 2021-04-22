6c49b7880b3bbc906d0fc0cf804800af38db0d08
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 19:45:33 +0900

    [Add] add mid property

--- src/presentation/controllers/motion-post-controller.ts
+++ src/presentation/controllers/motion-post-controller.ts
@@ -1,7 +1,7 @@
 import { AddMotion } from "../../domain/usecases";
 import { ServerError } from "../errors";
 import { ok, serverError } from "../helpers";
-import { Controller, HttpResponse } from "../protocols";
+import { Controller, HttpResponse, MiddlewareRequest } from "../protocols";
 
 export class MotionPostController implements Controller {
     constructor(
@@ -11,9 +11,11 @@ export class MotionPostController implements Controller {
         try {
             // request값 검사
             // db 추가
+            const file = request.mid.imageKey;  // 여기부분
             await this.addMotion.add({
                 ...(request),
                 time: Number(request.time),
+                file,
             });
             // ++ 파일 업로드할 때 미들웨어로 빼주기
             // 이미지 업로드 시 파일명 uuid 정해지기
@@ -28,7 +30,7 @@ export class MotionPostController implements Controller {
 
 export namespace MotionPostController {
     export type Request = {
-        file: string,
+        mid: MiddlewareRequest,
         name: string, // *
         time: string, // *
         description: string,
