de1e8c383359ad0a0ef686a1f41cf1930a3c1631
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 19:38:57 +0900

    [Add] add updateMotion interface

---
+++ src/domain/usecases/update-motion.ts
@@ -0,0 +1,19 @@
+import { MiddlewareRequest } from "../../presentation/protocols";
+
+export interface UpdateMotion {
+    update:(data: UpdateMotion.Params) => Promise<boolean>;
+}
+
+export namespace UpdateMotion {
+    export type Params = {
+        id: string,
+        file?: string,
+        name?: string,
+        time?: number,   // 정수로 변환
+        description?: string,
+        type?: string,
+        parts?: string[],
+        difficulty?: string,
+        url?: string
+    }
+}
