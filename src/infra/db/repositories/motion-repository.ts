0ad2e951a114e5d9f143a65aeddba1a44d338f14
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 19:40:36 +0900

    [Add] add updateMotionRepository in motion-repository

--- src/infra/db/repositories/motion-repository.ts
+++ src/infra/db/repositories/motion-repository.ts
@@ -1,12 +1,19 @@
-import { AddMotionRepository } from "../../../data/protocols/repository";
+import { AddMotionRepository, UpdateMotionRepository } from "../../../data/protocols/repository";
 import { MongoHelper } from "../mongo-helper";
 
 const Motion = MongoHelper.loadSchema('Motion');
 
-export class MotionRepository implements AddMotionRepository {
+export class MotionRepository implements AddMotionRepository, UpdateMotionRepository {
     async addMotion(data: AddMotionRepository.Params): Promise<void> {
         const input = new (await Motion)(data);
-        console.log(input);
+        // console.log(input); // test 로깅
         await input.save();
+    }
+
+    async updateMotion(data: UpdateMotionRepository.Params): Promise<boolean> {
+        const result = await (await Motion).findOneAndUpdate({ motion_id: data.id }, { ...(data) });
+        return !!result;
+        // 성공을 했다면 result에는 옛날 데이터가 나온다.
+        // 실패를 했다면 result에는 null 값이 넘겨진다.
     }
 }
