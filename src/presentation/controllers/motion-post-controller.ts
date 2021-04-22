c7be4b77156c4656e7d0829ebd033baa00902ae6
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 10:16:38 +0900

    [Add] add db add motion info

--- src/presentation/controllers/motion-post-controller.ts
+++ src/presentation/controllers/motion-post-controller.ts
@@ -1,15 +1,26 @@
+import { AddMotion } from "../../domain/usecases";
 import { ServerError } from "../errors";
 import { ok, serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
 export class MotionPostController implements Controller {
-    constructor() {}
+    constructor(
+        private readonly addMotion: AddMotion
+    ) {}
     async handle(request:MotionPostController.Request): Promise<HttpResponse> {
         try {
-            // 데코레이터 만들기
+            // request값 검사
+            // db 추가
+            await this.addMotion.add({
+                ...(request),
+                time: Number(request.time),
+            });
+            // ++ 파일 업로드할 때 미들웨어로 빼주기
+            // 이미지 업로드 시 파일명 uuid 정해지기
             return ok({});
         }
         catch(err) {
+            console.log('dddddddddddddd'); //test console loggin
             return serverError(new ServerError(err.stack));
         }
     }
@@ -17,6 +28,7 @@ export class MotionPostController implements Controller {
 
 export namespace MotionPostController {
     export type Request = {
+        file: string,
         name: string, // *
         time: string, // *
         description: string,
