2fa2b5634601341f984932fc3b68e15bf347bad1
pangcurry <magicshuffle2424@hotmail.com>
2021-04-22 10:18:03 +0900

    [Add] add file property in request object

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -3,10 +3,12 @@ import { Controller } from "../../presentation/protocols";
 
 export const adaptRoute = (controller: Controller) => {
     return async (req: Request, res: Response) => {
+        console.log(req.file);
         const request = {
             ...(req.body || {}),
             ...(req.params || {}),
-            ...(req.query || {})
+            ...(req.query || {}),
+            file: req.file["key"] || {},
         };
         const httpResponse = await controller.handle(request);
         const { status, statusCode, message, data } = httpResponse;
