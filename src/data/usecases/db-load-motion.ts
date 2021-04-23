e0ba2f3b8f7dcaa266a1b6cb3ae347bdf757a981
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 11:40:57 +0900

    [Add] add dbLoadMotion usecase

---
+++ src/data/usecases/db-load-motion.ts
@@ -0,0 +1,11 @@
+import { LoadMotion } from "../../domain/usecases";
+import { LoadMotionRepository } from "../protocols/repository";
+
+export class DbLoadMotion implements LoadMotion {
+    constructor(
+        private readonly loadMotionRepository: LoadMotionRepository
+    ) {}
+    async load(data: LoadMotion.Params): Promise<LoadMotionRepository.Result> {
+        
+    }
+}
--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './db-add-motion';
 export * from './db-update-motion';
 export * from './db-delete-motion';
+export * from './db-load-motion';
