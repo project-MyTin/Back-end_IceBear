2d05bde3e1072bf101a43ba19b8cb6ca15800240
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 11:12:24 +0900

    [Fix] fix return type boolean to string

--- src/data/usecases/db-delete-motion.ts
+++ src/data/usecases/db-delete-motion.ts
@@ -5,7 +5,7 @@ export class DbDeleteMotion implements DeleteMotion {
     constructor(
         private readonly deleteMotionRepository: DeleteMotionRepository
     ) {}
-    async delete(data: DeleteMotion.Params): Promise<boolean> {
+    async delete(data: DeleteMotion.Params): Promise<string> {
         const result = await this.deleteMotionRepository.deleteMotion({ id: data.id });
         return result;
     }
