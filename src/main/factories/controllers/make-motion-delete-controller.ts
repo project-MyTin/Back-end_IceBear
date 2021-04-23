50eda65cb57b098aed6a0fe05585fe255447c61e
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 09:32:24 +0900

    [Add] add dbDeleteMotion factory in motionDeleteController factory

--- src/main/factories/controllers/make-motion-delete-controller.ts
+++ src/main/factories/controllers/make-motion-delete-controller.ts
@@ -1,8 +1,9 @@
 import { MotionDeleteController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
 import { LogControllerDecorator } from "../../decorators";
+import { makeDbDeleteMotion } from "../usecases";
 
 export const makeMotionDeleteController = (): Controller => {
-    const controller = new MotionDeleteController();
+    const controller = new MotionDeleteController(makeDbDeleteMotion());
     return new LogControllerDecorator(controller);
 }
