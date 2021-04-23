add9fac4327f3dc661e823b49525da62d0a6ee76
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 09:32:42 +0900

    [Add] add motionDeleteController

--- src/presentation/controllers/motion-delete-controller.ts
+++ src/presentation/controllers/motion-delete-controller.ts
@@ -1,9 +1,22 @@
+import { DeleteMotion } from "../../domain/usecases/delete-motion";
+import { NotExistError, ServerError } from "../errors";
+import { notFound, ok, serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
 export class MotionDeleteController implements Controller {
-    constructor() {}
+    constructor(
+        private readonly deleteMotion: DeleteMotion
+    ) {}
     async handle(request: MotionDeleteController.Request): Promise<HttpResponse> {
-        
+        try {
+            const result = await this.deleteMotion.delete({ id: Number(request.id) });
+            if(!result) {
+                return notFound(new NotExistError());
+            }
+            return ok({});
+        } catch (err) {
+            return serverError(new ServerError(err.stack));
+        }
     }
 }
 
