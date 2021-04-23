b82562e34f493d9ab7b333c25de01014e2f6b8c2
pangcurry <magicshuffle2424@hotmail.com>
2021-04-23 14:06:46 +0900

    [Add] add dbLoadMotion factory in motionGetController factory

--- src/main/factories/controllers/make-motion-get-controller.ts
+++ src/main/factories/controllers/make-motion-get-controller.ts
@@ -1,8 +1,9 @@
 import { MotionGetController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
 import { LogControllerDecorator } from "../../decorators";
+import { makeDbLoadMotion } from "../usecases";
 
 export const makeMotionGetController = (): Controller => {
-    const controller = new MotionGetController();
+    const controller = new MotionGetController(makeDbLoadMotion());
     return new LogControllerDecorator(controller);
 }
