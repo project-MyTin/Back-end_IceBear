8a56c42c3e388728633ffe0356cfcb17ea1c2f65
pangcurry <magicshuffle2424@hotmail.com>
2021-05-24 08:50:19 +0900

    [Add] add DbAddRoutine class

---
+++ src/data/usecases/db-add-routine.ts
@@ -0,0 +1,10 @@
+import { AddRoutine } from "../../domain/usecases";
+
+export class DbAddRoutine implements AddRoutine {
+    constructor(
+        // private readonly routineRepository: 
+    ) {}
+    async add(data: AddRoutine.Params): Promise<void> {
+        
+    }
+}
