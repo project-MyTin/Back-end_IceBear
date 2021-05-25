396fcc400cac78452d0486eb396956e2e2490cb8
pangcurry <magicshuffle2424@hotmail.com>
2021-05-25 20:06:06 +0900

    [Add] add addRoutineRepository in routineRepository

--- src/infra/db/repositories/routine-repository.ts
+++ src/infra/db/repositories/routine-repository.ts
@@ -1,7 +1,11 @@
+import { AddRoutineRepository } from "../../../data/protocols/repository";
 import { MongoHelper } from "../mongo-helper";
 
 const Routine = MongoHelper.loadSchema('Routine');
 
-export class RoutineRepository {
-    
+export class RoutineRepository implements AddRoutineRepository {
+    async addRoutine(data: AddRoutineRepository.Params): Promise<void> {
+        const input = new (await Routine)(data);
+        await input.save();
+    }
 }
