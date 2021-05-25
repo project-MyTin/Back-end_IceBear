c0c454cae8bb347d1741728d30e1a5d8c9dbc39e
pangcurry <magicshuffle2424@hotmail.com>
2021-05-25 20:08:48 +0900

    [Update] update path

--- src/data/protocols/repository/index.ts
+++ src/data/protocols/repository/index.ts
@@ -3,3 +3,4 @@ export * from './update-motion-repository';
 export * from './delete-motion-repository';
 export * from './load-motion-repository';
 export * from './load-motion-result-repository';
+export * from './add-routine-repositroy';
