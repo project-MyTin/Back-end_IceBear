c22f84a1d79c78c95867ec0113556592245178f7
pangcurry <magicshuffle2424@hotmail.com>
2021-05-25 20:08:20 +0900

    [Add] add routinePostController

--- src/presentation/controllers/routine-post-controller.ts
+++ src/presentation/controllers/routine-post-controller.ts
@@ -1,11 +1,21 @@
+import { AddRoutine } from "../../domain/usecases";
 import { ServerError } from "../errors";
 import { ok, serverError } from "../helpers";
 import { Controller, HttpResponse, MiddlewareRequest } from "../protocols";
 
 export class RoutinePostController implements Controller {
-    constructor() {}
+    constructor(
+        private readonly addRoutine: AddRoutine
+    ) {}
     async handle(request: RoutinePostController.Request): Promise<HttpResponse> {
         try {
+            // 입력값 검사
+
+            // 루틴 저장
+            await this.addRoutine.add({
+                ...(request),
+                fileName: "request.mid.imageKey"
+            });
             return ok({});
             
         } catch (err) {
@@ -28,20 +38,7 @@ export namespace RoutinePostController {
         description: string,
         type: string,
         difficulty: string,
-        breakTime: string,
+        breakTime: number,
         motions: MotionInRoutine[]
     }
 }
-
-//     (- 루틴 아이디)
-// - 루틴 이름
-// - 루틴 준비물
-// - 루틴 설명
-// - 루틴 유형
-// - 루틴 난이도
-// - 루틴 쉬는시간
-// - 루틴 이미지
-// - 루틴 동작리스트
-// 	- 동작 아이디
-// 	- 동작 시간
-// 	- 동작 횟수
