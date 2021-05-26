e22d92232ff9699bd17b156ae3485a7ea995be2a
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 17:08:28 +0900

    [Add] add catch error in routineDeleteController

--- src/presentation/controllers/routine-delete-controller.ts
+++ src/presentation/controllers/routine-delete-controller.ts
@@ -1,9 +1,15 @@
+import { ServerError } from "../errors";
+import { serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
 export class RoutineDeleteController implements Controller {
     constructor() {}
     async handle(request: RoutineDeleteController.Request): Promise<HttpResponse> {
-        
+        try {
+            
+        } catch (err) {
+            return serverError(new ServerError(err.stack));
+        }
     }
 }
 
