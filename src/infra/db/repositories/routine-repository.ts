6b3b5bd47142821a21b5d5a71feaa107833c9510
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 16:54:35 +0900

    [Add] add updateRoutine func

--- src/infra/db/repositories/routine-repository.ts
+++ src/infra/db/repositories/routine-repository.ts
@@ -1,11 +1,16 @@
-import { AddRoutineRepository } from "../../../data/protocols/repository";
+import { AddRoutineRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
 import { MongoHelper } from "../mongo-helper";
 
 const Routine = MongoHelper.loadSchema('Routine');
 
-export class RoutineRepository implements AddRoutineRepository {
+export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository {
     async addRoutine(data: AddRoutineRepository.Params): Promise<void> {
         const input = new (await Routine)(data);
         await input.save();
+    }
+    async updateRoutine(data: UpdateRoutineRepository.Params): Promise<boolean> {
+        if(!data.file) { delete data.file }
+        const result = await (await Routine).findOneAndUpdate({ routine_id: data.id }, { ...data });
+        return !!result;
     }
 }
