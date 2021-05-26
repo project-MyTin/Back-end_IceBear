927885b9ab77ccb1b970c1a2e71e599cbd61ac75
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 19:44:48 +0900

    [Add] add deleteRoutine func in routineRepository

--- src/infra/db/repositories/routine-repository.ts
+++ src/infra/db/repositories/routine-repository.ts
@@ -1,9 +1,9 @@
-import { AddRoutineRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
+import { AddRoutineRepository, DeleteRoutineRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
 import { MongoHelper } from "../mongo-helper";
 
 const Routine = MongoHelper.loadSchema('Routine');
 
-export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository {
+export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository, DeleteRoutineRepository {
     async addRoutine(data: AddRoutineRepository.Params): Promise<void> {
         const input = new (await Routine)(data);
         await input.save();
@@ -12,5 +12,10 @@ export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRep
         if(!data.file) { delete data.file }
         const result = await (await Routine).findOneAndUpdate({ routine_id: data.id }, { ...data });
         return !!result;
+    }
+    async deleteRoutine(data: DeleteRoutineRepository.Params): Promise<string> {
+        const result = await (await Routine).findOneAndDelete({ routine_id: data.id });
+        if(!result) { return null; }
+        return result["file"];
     }
 }
