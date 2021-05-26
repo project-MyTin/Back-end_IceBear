58ef2e04b335d511edffbd279df69433d059551a
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 23:12:07 +0900

    [Add] add loadRoutine func in routineRepository

--- src/infra/db/repositories/routine-repository.ts
+++ src/infra/db/repositories/routine-repository.ts
@@ -1,9 +1,9 @@
-import { AddRoutineRepository, DeleteRoutineRepository, LoadRoutineResultRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
+import { AddRoutineRepository, DeleteRoutineRepository, LoadRoutineRepository, LoadRoutineResultRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
 import { MongoHelper } from "../mongo-helper";
 
 const Routine = MongoHelper.loadSchema('Routine');
 
-export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository, DeleteRoutineRepository, LoadRoutineResultRepository {
+export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository, DeleteRoutineRepository, LoadRoutineRepository, LoadRoutineResultRepository {
     async addRoutine(data: AddRoutineRepository.Params): Promise<void> {
         const input = new (await Routine)(data);
         await input.save();
@@ -17,6 +17,21 @@ export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRep
         const result = await (await Routine).findOneAndDelete({ routine_id: data.id });
         if(!result) { return null; }
         return result["file"];
+    }
+    async loadRoutine(data: LoadRoutineRepository.Params): Promise<LoadRoutineRepository.Result> {
+        const result = await (await Routine).findOne({ routine_id: data.id });
+        if(!result) { return null; }
+        return {
+            id: result["routine_id"],
+            file: result["file"],
+            name: result["name"],
+            materials: result["materials"],
+            description: result["description"],
+            type: result["type"],
+            difficulty: result["difficulty"],
+            break_time: result["break_time"],
+            motions: result["motions"],
+        };
     }
     async loadRoutineResult(data: LoadRoutineResultRepository.Params): Promise<LoadRoutineResultRepository.Result> {
         const list = await (await Routine).find({ name: { $regex: data.q } }).select(
