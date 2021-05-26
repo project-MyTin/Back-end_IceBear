fbd40fda52311e7836f04ad804a4e5069b10fea8
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 16:58:07 +0900

    [Add] add DbUpdateRoutine factory in routinePutController factory

--- src/main/factories/controllers/make-routine-put-controller.ts
+++ src/main/factories/controllers/make-routine-put-controller.ts
@@ -1,8 +1,9 @@
 import { RoutinePutController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
 import { makeLogControllerDecorator } from "../decorators";
+import { makeDbUpdateRoutine } from "../usecases";
 
 export const makeRoutinePutController = (): Controller => {
-    const controller = new RoutinePutController();
+    const controller = new RoutinePutController(makeDbUpdateRoutine());
     return makeLogControllerDecorator(controller);
 }
