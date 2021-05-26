d2a534f1269a130a7a6732874ae059918129f1fb
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 17:21:53 +0900

    [Add] add dbDeleteRoutine factory in routineDeleteController factory

--- src/main/factories/controllers/make-routine-delete-controller.ts
+++ src/main/factories/controllers/make-routine-delete-controller.ts
@@ -1,8 +1,9 @@
 import { RoutineDeleteController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
 import { makeLogControllerDecorator } from "../decorators";
+import { makeDbDeleteRoutine } from "../usecases";
 
 export const makeRoutineDeleteController = (): Controller => {
-    const controller = new RoutineDeleteController();
+    const controller = new RoutineDeleteController(makeDbDeleteRoutine());
     return makeLogControllerDecorator(controller);
 }
