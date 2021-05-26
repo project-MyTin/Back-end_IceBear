7649b8e5aea0f77ccd1267c5685dfc99f6521e20
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 10:12:07 +0900

    [Update] update path

--- src/main/factories/controllers/index.ts
+++ src/main/factories/controllers/index.ts
@@ -4,3 +4,4 @@ export * from './make-motion-delete-controller';
 export * from './make-motion-get-controller';
 export * from './make-motion-search-controller';
 export * from './make-routine-post-controller';
+export * from './make-routine-put-controller';
