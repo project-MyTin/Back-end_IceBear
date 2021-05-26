b89b4fdc286cbb9f34424b7857fb4cb373744c17
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 18:53:05 +0900

    [Add] add /routine/:id delete

--- src/main/routes/routine.ts
+++ src/main/routes/routine.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
 import { upload } from "../config/multer";
-import { makeRoutinePostController, makeRoutinePutController } from "../factories";
+import { makeRoutineDeleteController, makeRoutinePostController, makeRoutinePutController } from "../factories";
 import { uploadSingle } from "../middlewares";
 import { uploadSingleUpdate } from "../middlewares/upload-single-update";
 
 export default (router: Router): void => {
     router.post('/routine', upload.single("img"), uploadSingle, adaptRoute(makeRoutinePostController()));
     router.put('/routine/:id', upload.single("img"), uploadSingleUpdate, adaptRoute(makeRoutinePutController()));
+    router.delete('/routine/:id', adaptRoute(makeRoutineDeleteController()));
 }
