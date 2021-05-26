b6dcd536a4d6e01c46386fef7ca9f6b7c554958f
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 19:43:38 +0900

    [Add] add deleteRoutine and s3Delete in routineDeleteController

--- src/presentation/controllers/routine-delete-controller.ts
+++ src/presentation/controllers/routine-delete-controller.ts
@@ -1,15 +1,24 @@
+import { S3Delete } from "../../data/S3";
 import { DeleteRoutine } from "../../domain/usecases";
-import { ServerError } from "../errors";
-import { ok, serverError } from "../helpers";
+import { ImageNotExistError, NotExistError, ServerError } from "../errors";
+import { notFound, ok, serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
 export class RoutineDeleteController implements Controller {
     constructor(
-        private readonly deleteRoutine: DeleteRoutine
+        private readonly deleteRoutine: DeleteRoutine,
+        private readonly s3Delete: S3Delete
     ) {}
     async handle(request: RoutineDeleteController.Request): Promise<HttpResponse> {
         try {
-
+            const key = await this.deleteRoutine.delete({ id: Number(request.id) });
+            if(!key) {
+                return notFound(new NotExistError());
+            }
+            const result = await this.s3Delete.delete(key);
+            if(!result) {
+                return notFound(new ImageNotExistError());
+            }
             return ok({});
         } catch (err) {
             return serverError(new ServerError(err.stack));
