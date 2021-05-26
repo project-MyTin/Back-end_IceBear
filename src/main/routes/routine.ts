fd0de119d024bb6cf97629332c3b5253988833bf
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 22:04:19 +0900

    [Add] add /routine get

--- src/main/routes/routine.ts
+++ src/main/routes/routine.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
 import { upload } from "../config/multer";
-import { makeRoutineDeleteController, makeRoutinePostController, makeRoutinePutController } from "../factories";
+import { makeRoutineDeleteController, makeRoutinePostController, makeRoutinePutController, makeRoutineSearchController } from "../factories";
 import { uploadSingle } from "../middlewares";
 import { uploadSingleUpdate } from "../middlewares/upload-single-update";
 
@@ -9,4 +9,5 @@ export default (router: Router): void => {
     router.post('/routine', upload.single("img"), uploadSingle, adaptRoute(makeRoutinePostController()));
     router.put('/routine/:id', upload.single("img"), uploadSingleUpdate, adaptRoute(makeRoutinePutController()));
     router.delete('/routine/:id', adaptRoute(makeRoutineDeleteController()));
+    router.get('/routine', adaptRoute(makeRoutineSearchController()));
 }
