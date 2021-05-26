39704ebef635008a7e4390ba9250c4482b33bdd3
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 10:01:31 +0900

    [Add] add uploadSingle middlewares

--- src/main/routes/routine.ts
+++ src/main/routes/routine.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
+import { upload } from "../config/multer";
 import { makeRoutinePostController } from "../factories";
+import { uploadSingle } from "../middlewares";
 
 export default (router: Router): void => {
-    router.post('/routine', adaptRoute(makeRoutinePostController()));
+    router.post('/routine', upload.single("img"), uploadSingle, adaptRoute(makeRoutinePostController()));
 }
