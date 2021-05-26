0cc4fac0f3cff1f2b38e09263a8562c0fce48716
pangcurry <magicshuffle2424@hotmail.com>
2021-05-26 22:02:04 +0900

    [Add] add loadRoutineResult func in routineRepository

--- src/infra/db/repositories/routine-repository.ts
+++ src/infra/db/repositories/routine-repository.ts
@@ -1,9 +1,9 @@
-import { AddRoutineRepository, DeleteRoutineRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
+import { AddRoutineRepository, DeleteRoutineRepository, LoadRoutineResultRepository, UpdateRoutineRepository } from "../../../data/protocols/repository";
 import { MongoHelper } from "../mongo-helper";
 
 const Routine = MongoHelper.loadSchema('Routine');
 
-export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository, DeleteRoutineRepository {
+export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRepository, DeleteRoutineRepository, LoadRoutineResultRepository {
     async addRoutine(data: AddRoutineRepository.Params): Promise<void> {
         const input = new (await Routine)(data);
         await input.save();
@@ -17,5 +17,11 @@ export class RoutineRepository implements AddRoutineRepository, UpdateRoutineRep
         const result = await (await Routine).findOneAndDelete({ routine_id: data.id });
         if(!result) { return null; }
         return result["file"];
+    }
+    async loadRoutineResult(data: LoadRoutineResultRepository.Params): Promise<LoadRoutineResultRepository.Result> {
+        const list = await (await Routine).find({ name: { $regex: data.q } }).select(
+            "-_id routine_id name difficulty description type file"
+        );
+        return list as any;
     }
 }
