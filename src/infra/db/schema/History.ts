caf5690e9efc63630c6add3373e99ebfdcf59dc6
pangcurry <magicshuffle2424@hotmail.com>
2021-05-30 22:37:07 +0900

    [Add] add history schema

---
+++ src/infra/db/schema/History.ts
@@ -0,0 +1,37 @@
+import { Schema } from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
+
+const History = new Schema({
+    history_id: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    timestemp: {
+        type: String,
+        require: true,
+    },
+    day: {
+        type: Number,
+    },
+    routine_list: {
+        type: [String]
+    },
+    routine_total_time: {
+        type: Number,
+    },
+},{
+    timestamps: {
+        createdAt: true,
+        updatedAt: true,
+    }
+});
+
+History.plugin(autoIncrement.plugin, {
+    model: 'History',
+    field: 'history_id',
+    startAt: 1,
+    increment: 1,
+});
+
+export default History;
