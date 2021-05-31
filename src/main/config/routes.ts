578b26a5b3e87b797d9174c5ec0f1d45703e103b
pangcurry <magicshuffle2424@hotmail.com>
2021-05-31 11:13:14 +0900

    [Add] add calendarRoutes in config routines

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,6 +3,7 @@ import { Express, Router } from 'express';
 import pingRoutes from '../routes/ping';
 import motionRoutes from '../routes/motion';
 import routineRoutes from '../routes/routine';
+import calendarRoutes from '../routes/calendar';
 
 const router = Router();
 
@@ -12,4 +13,5 @@ export default (app: Express): void => {
     pingRoutes(router);
     motionRoutes(router);
     routineRoutes(router);
+    calendarRoutes(router);
 }
