0feb6a6d3194b71e5b8b3a412f2bb654c4f2a3da
pangcurry <magicshuffle2424@hotmail.com>
2021-05-31 23:45:32 +0900

    [Fix] fix type

--- src/data/protocols/repository/add-history-repository.ts
+++ src/data/protocols/repository/add-history-repository.ts
@@ -5,5 +5,22 @@ export class AddHistoryRepository {
 }
 
 export namespace AddHistoryRepository {
-    export type Params = AddHistory.Params;
+    export type Routine = {
+        routine_id: number,
+        routine_name: string,
+        routine_file: string,
+        routine_time: number,
+    };
+    export type Motion = {
+        motion_id: number,
+        motion_name: string,
+        motion_file: string,
+        motion_time: number,
+    };
+    export type Params = {
+        total_time: number,
+        motion_num: number,
+        motion_list: Motion[],
+        routine: Routine,        
+    };
 }
--- src/infra/db/schema/History.ts
+++ src/infra/db/schema/History.ts
@@ -15,11 +15,33 @@ const History = new Schema({
         type: Number,
     },
     routine_list: {
-        type: [String]
+        type: [{
+            routine_id: { type: Number },
+            routine_name: { type: String },
+            routine_file: { type: String },
+            routine_time: { type: Number },
+        }]
+    },
+    motion_list: {
+        type: [{
+            motion_id: { type: Number },
+            motion_name: { type: String },
+            motion_file: { type: String },
+            motion_time: { type: Number },
+        }]
     },
     routine_total_time: {
         type: Number,
     },
+    routine_num: {  // 필요한가 ?
+        type: Number,
+    },
+    motion_num: {
+        type: Number,
+    },
+    exp: {
+        type: Number,
+    }
 },{
     timestamps: {
         createdAt: true,
